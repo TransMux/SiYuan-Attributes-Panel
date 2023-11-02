2a6816ae249d54c911e54b423a59e280130df76d
Mux <siyuan@transmux.world>
2023-11-03 01:57:25 +0800

    监听右键shift失败，内置的优先级太高了

--- src/views/AttributePanel.vue
+++ src/views/AttributePanel.vue
@@ -13,15 +13,33 @@
 <script setup lang="ts">
 import AttributeRow from '@/components/AttributeRow.vue';
 import { useAttributesStore } from '@/store/attribute';
-import { computed, onMounted } from 'vue';
+import { onMounted } from 'vue';
 // 通过一个块id，渲染对应的属性面板
 
 const attributeStore = useAttributesStore();
 
-
 onMounted(() => {
     attributeStore.fetchAttributes();
+    window.addEventListener("click", handleShiftClick, false);
 });
+
+// eventListener: listen for shift + left click on class="protyle-gutters"
+function handleShiftClick(e: MouseEvent) {
+    if (!e.shiftKey) {
+        return;
+    }
+    console.log("### shift + click", e.target);
+    const target = e.target as HTMLElement;
+    if (target.classList.contains("protyle-gutters")) {
+        // find button in target, get data-node-id
+        const button = target.getElementsByTagName("button")[0];
+        const nodeId = button.dataset.nodeId;
+        if (nodeId) {
+            attributeStore.inspectBlock(nodeId);
+            e.stopPropagation();
+        }
+    }
+}
 </script>
 
 <style scoped>
