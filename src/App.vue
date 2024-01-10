ec112389738561d54fccdf51a84f6c60f4442278
Mux <siyuan@transmux.world>
2024-01-10 12:05:44 +0800

    init panel when vue load

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </template>
 
 <script setup lang="ts">
+import { useAttributesStore } from './store/attribute';
 import AttributePanel from './views/AttributePanel.vue';
+
+const attributeStore = useAttributesStore();
+attributeStore.fetchInnerAttributes();
 </script>
 
 <style scoped></style>
